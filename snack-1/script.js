709237ea04cb8474593a8a27cb07c80f2e49445b
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-21 15:47:00 +0100

    repo completata

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+names.forEach(person => {
+    console.log(person);
+});
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
---
+++ snack-5/script.js
@@ -0,0 +1,13 @@
+const nums = [2, 8, 4, 7, 12, 87];
+
+const numsPari = nums.filter((number) => {
+    if(number % 2 === 0) {
+    return true;
+    }
+    return false
+});
+
+console.log(numsPari);
+
+// Crea un array a partire dall'array nums, che abbia solo i numeri pari
+// Risultato: [2, 8, 4, 12]
